97f3fe01d3483d147d18cbaabbfdacb9a94b02da
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-11-01 19:28:45 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,55 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?' +
+  `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      reviews: []
+    }
+  }
+
+    componentDidMount() {
+      console.warn('Latest Did Mount')
+      this.getFetch()
+    }
+
+
+  getFetch() {
+      fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            reviews: data.results
+          })
+        })
+    }
+
+
+
+      render() {
+
+        return ( < div className = "latest-movie-reviews" ><h1>Latest Reviews:</h1>
+                    <br />
+
+                            <MovieReviews reviews={this.state.reviews}/>
+                
+                < /div>)
+
+
+        }
+
+
+      }
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,66 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            reviews:[],
+            searchTerm: []
+        }
+    }
+
+  getFetch() {
+      fetch(URL + `&query=${this.state.searchTerm}`)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            reviews: data.results
+          })
+        })
+    }
+
+    handleSearch = (event) => {
+        const searchT = event.currentTarget.value;
+        this.setState({
+            searchTerm: searchT
+        })
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.getFetch();
+    }
+
+
+    render() {
+
+        return (<div className="searchable-movie-reviews"><h1>Search results: </h1>
+        <form onSubmit={ this.handleSubmit }>
+        <input type="text" name="search" onChange={this.handleSearch}
+        value={this.state.searchTerm} />
+          <button type="submit">Submit</button>
+        </form>
+
+        <div>
+        <MovieReviews reviews={this.state.reviews}/>
+        </div>
+        <br /><br />
+        </div>);
+
+    }
+
+
+
+}
